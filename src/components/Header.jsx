1362a7d43879b9ff8f6c172977e8e2d63448f281
Abraham Espinoza <abraham.espinoza@optimen.com.mx>
2023-11-18 23:45:03 -0600

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,27 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, Text, IconButton, useMediaQuery } from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
 
 export function Header() {
+  const [isLargerThan768] = useMediaQuery('(min-width: 768px)');
+
   return (
     <Box
       bg='#1E392A'
       p={8}
       borderBottomLeftRadius={30}
-      borderBottomRightRadius={30}>
-      <Flex>
-        <Text>Hola</Text>
+      borderBottomRightRadius={30}
+    >
+      <Flex justify="space-between" align="center">
+        {isLargerThan768 ? (
+          <Text fontSize="xl" color="white">Voz pasiva</Text>
+        ) : (
+          <IconButton
+            icon={<HamburgerIcon />}
+            variant="outline"
+            colorScheme="white"
+          />
+        )}
       </Flex>
     </Box>
-  )
+  );
 }
